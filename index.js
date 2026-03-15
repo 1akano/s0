a9385329b3c1991fa767e78d4f59740625c15e15
1akano <0611sep@gmail.com>
2026-03-15 14:27:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var a = require('ws');
 var b = new a.Server({ port: 8080 });
 b.on('connection', (c) => {
     c.on('message', (d) => {
-        c.send(d);
+        b.clients.forEach((e) => {
+            if (e.readyState === a.OPEN) {
+                e.send(d);
+            }
+        });
     });
 });
